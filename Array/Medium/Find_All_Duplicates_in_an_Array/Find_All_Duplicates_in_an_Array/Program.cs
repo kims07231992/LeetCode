62a85c26b5ac0329590bc05e7b79231d73ea1906
Jason Kim <kims07231992@gmail.com>
2019-11-20 01:55:38 -0800

    Solved Find_All_Duplicates_in_an_Array

---
+++ Array/Medium/Find_All_Duplicates_in_an_Array/Find_All_Duplicates_in_an_Array/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Find_All_Duplicates_in_an_Array
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Run();
+        }
+
+        private static void Run()
+        {
+            var nums = new int[] { 4, 3, 2, 7, 8, 2, 3, 1 };
+            var duplicates = FindDuplicates(nums);
+        }
+
+        /// <summary>
+        /// Time Complexity: O(N) where N is length of nums
+        /// Space Complexity => X
+        /// </summary>
+        private static IList<int> FindDuplicates(int[] nums)
+        {
+            var result = new List<int>();
+
+            for (int i = 0; i < nums.Length; ++i)
+            {
+                int index = Math.Abs(nums[i]) - 1;
+                if (nums[index] < 0)
+                    result.Add(index + 1);
+
+                nums[index] = -nums[index];
+            }
+
+            return result;
+        }
+    }
+}
