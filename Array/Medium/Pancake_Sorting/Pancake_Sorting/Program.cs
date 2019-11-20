e8cbdbc07f455905d431fd1fbcda2d70974da9d6
Jason Kim <kims07231992@gmail.com>
2019-11-19 20:13:42 -0800

    Solved Pancake Sorting

---
+++ Array/Medium/Pancake_Sorting/Pancake_Sorting/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+
+namespace Pancake_Sorting
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Run();
+        }
+
+        private static void Run()
+        {
+            var input = new int[] { 3, 2, 4, 1 };
+            var output = PancakeSort(input);
+        }
+            
+        private static IList<int> PancakeSort(int[] A)
+        {
+            var result = new List<int>();
+            for (int key = A.Length; key > 0; key--)
+            {
+                for (int index = key - 1; index >= 0; index--)
+                {
+                    if (key == A[index])
+                    {
+                        Reverse(A, index);
+                        result.Add(index + 1);
+                        Reverse(A, key - 1);
+                        result.Add(key);
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        private static void Reverse(int[] A, int k)
+        {
+            for (int i = 0, j = k; i < j; i++, j--)
+            {
+                int temp = A[i];
+                A[i] = A[j];
+                A[j] = temp;
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -6,3 +6,4 @@
 
 * [Reveal Cards In Increasing Order](https://github.com/kims07231992/LeetCode/tree/master/Array/Medium/Reveal_Cards_In_Increasing_Order)
 * [Queens That Can Attack the King](https://github.com/kims07231992/LeetCode/tree/master/Array/Medium/Queens_That_Can_Attack_the_King)
+* [Pancake Sorting](https://github.com/kims07231992/LeetCode/tree/master/Array/Medium/Pancake_Sorting)
