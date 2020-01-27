964d6e4ca3c7c4357f06bf95e2c1568b1243bb4c
Jason Kim <kims07231992@gmail.com>
2020-01-26 20:26:45 -0800

    Sovled Integer to Roman

---
+++ Math/Medium/Integer_to_Roman/Integer_to_Roman/Program.cs
@@ -0,0 +1,31 @@
+﻿
+namespace Integer_to_Roman
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Run();
+        }
+
+        private static void Run()
+        {
+            var num = 9;
+            var result = IntToRoman(num);
+        }
+
+        /// <summary>
+        /// Time Complexity: O(1)
+        /// Space Complexity: O(1)
+        /// </summary>
+        private static string IntToRoman(int num)
+        {
+            var M = new string[] { "", "M", "MM", "MMM" }; // 0, 1000, 2000, 3000
+            var C = new string[] { "", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM" }; // 0, 100, 200, 300, 400, 500, 600, 700, 800, 900
+            var X = new string[] { "", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC" }; // 0, 10, 20, 30, 40, 50, 60, 70, 80, 90
+            var I = new string[] { "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" }; // 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+
+            return M[num / 1000] + C[(num % 1000) / 100] + X[(num % 100) / 10] + I[num % 10];
+        }
+    }
+}
--- README.md
+++ README.md
@@ -58,6 +58,7 @@
 
 ### Math
 
+* [Integer to Roman](https://github.com/kims07231992/LeetCode/tree/master/Math/Medium/Integer_to_Roman)
 * [String to Integer (atoi)](https://github.com/kims07231992/LeetCode/tree/master/Math/Medium/String_to_Integer)
 
 
