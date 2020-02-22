5e34d2fb4f883812007b1a4b351f0465e04464f2
Jason Kim <kims07231992@gmail.com>
2020-02-21 19:14:18 -0800

    Solved Search a 2D Matrix II

---
+++ Online_Assessment_Amazon/Search_a_2D_Matrix_II/Search_a_2D_Matrix_II/Program.cs
@@ -0,0 +1,54 @@
+﻿
+namespace Search_a_2D_Matrix_II
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Run();
+        }
+
+        private static void Run()
+        {
+            var matrix = new int[,]
+            {
+                { 1, 4, 7, 11, 15 },
+                { 2, 5, 8, 12, 19 },
+                { 3, 6, 9, 16, 22 },
+                { 10, 13, 14, 17, 24 },
+                { 18, 21, 23, 26, 30 }
+            };
+            var target = 20;
+
+            var result = SearchMatrix(matrix, target);
+        }
+
+        /// <summary>
+        /// Time Complexity: O(M + N) where M is number of rows and N is number of columns
+        /// Space Complexity: O(1)
+        /// </summary>
+        private static bool SearchMatrix(int[,] matrix, int target)
+        {
+            if (matrix == null || matrix.Length < 1)
+                return false;
+
+            var m = matrix.GetLength(0);
+            var n = matrix.Length / m;
+
+            int i = 0;
+            int j = n - 1;
+            while (i < m && j >= 0)
+            {
+                int current = matrix[i, j];
+                if (current == target)
+                    return true;
+                else if (current < target)
+                    i++;
+                else if (current > target)
+                    j--;
+            }
+
+            return false;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -144,6 +144,7 @@
 
 * [Copy List with Random Pointer](https://github.com/kims07231992/LeetCode/tree/master/Online_Assessment_Amazon/Copy_List_with_Random_Pointer)
 * [Reorder Data in Log Files](https://github.com/kims07231992/LeetCode/tree/master/Online_Assessment_Amazon/Reorder_Data_in_Log_Files)
+* [Search a 2D Matrix II](https://github.com/kims07231992/LeetCode/tree/master/Online_Assessment_Amazon/Search_a_2D_Matrix_II)
 * [Search Suggestions System](https://github.com/kims07231992/LeetCode/tree/master/Online_Assessment_Amazon/Search_Suggestions_System)
 * [Subtree of Another Tree](https://github.com/kims07231992/LeetCode/tree/master/Online_Assessment_Amazon/Subtree_of_Another_Tree)
 * [Top N Buzzwords](https://github.com/kims07231992/LeetCode/tree/master/Online_Assessment_Amazon/Top_N_Buzzwords)
