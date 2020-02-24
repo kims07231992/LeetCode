fd3813e85defe9e674929ca926a1a525b3519270
Jason Kim <kims07231992@gmail.com>
2020-02-24 02:13:08 -0800

    Solved Two Sum Unique Pairs

---
+++ Online_Assessment_Amazon/Two_Sum_Unique_Pairs/Two_Sum_Unique_Pairs/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+
+namespace Two_Sum_Unique_Pairs
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Run();
+        }
+
+        private static void Run()
+        {
+            var nums = new int[] { 1, 1, 2, 45, 46, 46 };
+            var target = 47;
+
+            var result = TwoSumUniquePairs(nums, target);
+        }
+
+        /// <summary>
+        /// Time Complexity: O(N) where N is length of given array nums
+        /// Space Complexity: O(N)
+        /// </summary>
+        private static int TwoSumUniquePairs(int[] nums, int target)
+        {
+            var set = new HashSet<int>();
+            var seen = new HashSet<int>();
+            var count = 0;
+            foreach (int num in nums)
+            {
+                int remain = target - num;
+                set.Add(num);
+                if (set.Contains(remain) && !seen.Contains(num))
+                {
+                    seen.Add(num);
+                    seen.Add(remain);
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
