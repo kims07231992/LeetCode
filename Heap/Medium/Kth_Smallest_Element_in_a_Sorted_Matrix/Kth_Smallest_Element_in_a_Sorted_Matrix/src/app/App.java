27632221a30605369a267b1949505d663ad61711
Jason Kim <kims07231992@gmail.com>
2020-02-29 18:03:43 -0800

    Converted Kth_Smallest_Element_in_a_Sorted_Matrix to Java

--- Heap/Medium/Kth_Smallest_Element_in_a_Sorted_Matrix/Kth_Smallest_Element_in_a_Sorted_Matrix/src/app/App.java
+++ Heap/Medium/Kth_Smallest_Element_in_a_Sorted_Matrix/Kth_Smallest_Element_in_a_Sorted_Matrix/src/app/App.java
@@ -1,7 +1,38 @@
 package app;
 
+import java.util.PriorityQueue;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello Java");
+    public static void main(String[] args)  {
+        run();
+    }
+
+    private static void run() {
+        int[][] matrix= new int[][] { 
+            { 1,  5,  9 },
+            { 10, 11, 13 },
+            { 12, 13, 15 }
+        };
+        int k = 8;
+        int result = kthSmallest(matrix, k);
+    }
+
+    /// <summary>
+    /// Time Complexity: O(NlogK) where N is size of matrix and K is given integer k
+    /// Space Complexity: O(N)
+    /// </summary>
+    private static int kthSmallest(int[][] matrix, int k) {
+        int n = matrix.length;
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>((x, y) -> y - x);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < Math.min(n, k); j++) {
+                pq.offer(matrix[i][j]);
+                
+                if (pq.size() > k)
+                    pq.poll();
+            }
+        }
+        
+        return pq.peek();
     }
 }
