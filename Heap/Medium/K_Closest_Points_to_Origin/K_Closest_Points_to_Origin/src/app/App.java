423467f6e38fd8212f94d2170ce622cbf474357d
Jason Kim <kims07231992@gmail.com>
2020-02-29 18:09:17 -0800

    Reverted unintended commit

--- Heap/Medium/K_Closest_Points_to_Origin/K_Closest_Points_to_Origin/src/app/App.java
+++ Heap/Medium/K_Closest_Points_to_Origin/K_Closest_Points_to_Origin/src/app/App.java
@@ -6,29 +6,33 @@ public class App {
     public static void main(String[] args) {
         run();
     }
-
+    
     private static void run() {
-        int[][] matrix = new int[][] { { 3, 3 }, { 5, -1 }, { -2, 4 } };
+        int[][] points = new int[][] { { 3, 3 }, { 5, -1 }, { -2, 4 } };
         int K = 2;
-        int result = kthSmallest(matrix, K);
+        int[][] result = kClosest(points, K);
     }
 
     /// <summary>
-    /// Time Complexity: O(NlogZ) where Z is Min(N, K)
-    /// Space Complexity: O(K)
+    /// Time Complexity: O(NlogK) where N is size of given integer num and K is given integer K
+    /// Space Complexity: O(N)
     /// </summary>
-    private static int kthSmallest(int[][] matrix, int k) {
-        int n = matrix.length;
-        PriorityQueue<Integer> pq = new PriorityQueue<Integer>((x, y) -> y - x);
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < Math.min(n, k); j++) {
-                pq.offer(matrix[i][j]);
-                
-                if (pq.size() > k)
-                    pq.poll();
-            }
+    private static int[][] kClosest(int[][] points, int K) {        
+        int[][] result = new int[K][];
+        PriorityQueue<int[]> pq = new PriorityQueue<int[]> // max Heap
+            ((p1, p2) -> p2[0] * p2[0] + p2[1] * p2[1] - p1[0] * p1[0] - p1[1] * p1[1]);
+
+        for (int i = 0; i < points.length; i++) {            
+            pq.offer(points[i]);
+
+            if (pq.size() > K)
+                pq.poll();
         }
-        
-        return pq.peek();
+
+        for (int i = 0; i < K; i++) {
+            result[i] = pq.poll();
+        }
+
+        return result;
     }
 }
