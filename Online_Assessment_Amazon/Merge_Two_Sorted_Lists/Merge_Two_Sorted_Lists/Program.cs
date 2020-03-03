d147d3d87286ca9c1648222af20a5d1cc3bc4954
Jason Kim <kims07231992@gmail.com>
2020-03-03 00:36:56 -0800

    Solved Merge Two Sorted Lists

---
+++ Online_Assessment_Amazon/Merge_Two_Sorted_Lists/Merge_Two_Sorted_Lists/Program.cs
@@ -0,0 +1,58 @@
+﻿
+namespace Merge_Two_Sorted_Lists
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Run();
+        }
+
+        private static void Run()
+        {
+            var l1 = new ListNode(1);
+            l1.next = new ListNode(2);
+            l1.next.next = new ListNode(4);
+
+            var l2 = new ListNode(1);
+            l2.next = new ListNode(3);
+            l2.next.next = new ListNode(4);
+
+            var result = MergeTwoLists(l1, l2);
+        }
+
+        /// <summary>
+        /// Time Complexity: O(N + M) where N is length of l1 M is length of l2
+        /// Space Complexity: O(1)
+        /// </summary>
+        private static ListNode MergeTwoLists(ListNode l1, ListNode l2)
+        {
+            var dummy = new ListNode(0);
+            var current = dummy;
+            while (l1 != null && l2 != null)
+            {
+                if (l1.val <= l2.val)
+                {
+                    current.next = l1;
+                    l1 = l1.next;
+                }
+                else
+                {
+                    current.next = l2;
+                    l2 = l2.next;
+                }
+                current = current.next;
+            }
+            current.next = l1 != null ? l1 : l2;
+
+            return dummy.next;
+        }
+
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int x) { val = x; }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -146,6 +146,7 @@
 * [Critical Connections](https://github.com/kims07231992/LeetCode/tree/master/Online_Assessment_Amazon/Critical_Connections)
 * [Favorite Genres](https://github.com/kims07231992/LeetCode/tree/master/Online_Assessment_Amazon/Favorite_Genres)
 * [Find Pair With Given Sum](https://github.com/kims07231992/LeetCode/tree/master/Online_Assessment_Amazon/Find_Pair_With_Given_Sum)
+* [Merge Two Sorted Lists](https://github.com/kims07231992/LeetCode/tree/master/Online_Assessment_Amazon/Merge_Two_Sorted_Lists)
 * [Path Sum](https://github.com/kims07231992/LeetCode/tree/master/Online_Assessment_Amazon/Path_Sum)
 * [Reorder Data in Log Files](https://github.com/kims07231992/LeetCode/tree/master/Online_Assessment_Amazon/Reorder_Data_in_Log_Files)
 * [Search a 2D Matrix II](https://github.com/kims07231992/LeetCode/tree/master/Online_Assessment_Amazon/Search_a_2D_Matrix_II)
