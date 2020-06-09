72617e16145d9e552bee4f932231623d423c1470
Jason Kim <kims07231992@gmail.com>
2020-06-08 22:36:14 -0700

    Solve Lowest Common Ancestor of a Binary Tree

---
+++ Tree/Medium/Lowest_Common_Ancestor_of_a_Binary_Tree/Lowest_Common_Ancestor_of_a_Binary_Tree/Program.cs
@@ -0,0 +1,49 @@
+﻿
+namespace Lowest_Common_Ancestor_of_a_Binary_Tree
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Run();
+        }
+
+        private static void Run()
+        {
+            var root = new TreeNode(3);
+            root.left = new TreeNode(5);
+            root.right = new TreeNode(1);
+            root.left.left = new TreeNode(6);
+
+            var result = LowestCommonAncestor(root, root.left, root.right);
+        }
+
+        /// <summary>
+        /// Time Complexity: O(N) where N is number of elements from given tree
+        /// Space Complexity: O(N)
+        /// </summary>
+        private static TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+        {
+            if (root == null || root == p || root == q)
+                return root;
+
+            var left = LowestCommonAncestor(root.left, p, q);
+            var right = LowestCommonAncestor(root.right, p, q);
+
+            if (left == null)
+                return right;
+            else if (right == null)
+                return left;
+            else
+                return root;
+        }
+
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int x) { val = x; }
+        }
+    }
+}
